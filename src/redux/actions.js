827531502ba5afd6424c7880a54cf824998e247a
Md Rabby Hasan <golamrabbynwu@gmail.com>
2024-05-20 00:19:30 +0600

    React_Redux_todo app

---
+++ src/redux/actions.js
@@ -0,0 +1,42 @@
+import { ADD_TODO, FILTER_TODO, MARK_ALL_COMPLETED, MARK_COMPLETED, MARK_INCOMPLETED, REMOVE_TODO, TOGGLE_TODO, UPDATE_SEARCH_TERM } from "./actionTypes"
+
+export const addTodo = (text) =>({
+    type:ADD_TODO,
+    payload:{text}
+});
+
+export const togleToDo =(id)=>({
+    type:TOGGLE_TODO,
+    payload:{id}
+});
+
+export const removeToDo =(id)=>({
+    type:REMOVE_TODO,
+    payload:{id}
+});
+
+export const markCompleted = (id)=>({
+    type:MARK_COMPLETED,
+    payload:{id}
+});
+
+export const markIncomplete =(id)=>({
+    type:MARK_INCOMPLETED,
+    payload:{id}
+});
+
+export const filterTodos = (filter) => ({
+    type: FILTER_TODO,
+    payload: {filter}
+})
+
+export const markAllCompleted = ()=>({
+    type:MARK_ALL_COMPLETED,
+});
+
+export const updateSearchTerm = (search) =>({
+    type:UPDATE_SEARCH_TERM,
+    payload :{search}
+})
+
+
